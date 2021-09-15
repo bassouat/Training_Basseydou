99a828f5d00ae9f783540124adc3557a365c13bd
Ouattara Basseydou <bassouat8@gmail.com>
2021-09-15 16:25:48 +0200

    établissement du routing avec le router

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
 
+const stuffRoutes = require('./routes/stuff');
 const app = express();
+
+
 
 // mongodb://uveq9m1ypwyl8akr9piu:FwvJh88Rest25GD7huVA@btqphkqnwhbsvjm-mongodb.services.clever-cloud.com:27017/btqphkqnwhbsvjm
 mongoose.connect('mongodb+srv://basseydou:reussite2021@cluster0.eor5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -29,39 +31,7 @@ app.use((req, res, next) => {
     
 // });
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-  .then(things => res.status(200).json(things))
-  .catch(error => res.status(400).json({ error }));
-  });
-
+app.use('/api/stuff', stuffRoutes);
 
 module.exports=app;
 
