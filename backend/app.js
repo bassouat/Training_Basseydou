9dca2444fea010832676a0db220f5544342e9e7c
Ouattara Basseydou <bassouat8@gmail.com>
2021-09-15 03:20:58 +0200

    Mise en place BDD Mongodb et creation schema de donnees

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://basseydou:reussite2021@cluster0.eor5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
